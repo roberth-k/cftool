2520d3c8d3b52c210ec3508e80cdef1c9291c654
Roberth Kulbin <roberth@winter-weht.net>
2019-07-14 16:05:44 +0100

    internal/cli: add deploy framework

---
+++ internal/cli/deploy.go
@@ -0,0 +1,76 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/pkg/errors"
+	"github.com/tetratom/cftool/internal"
+	"github.com/tetratom/cftool/pkg/cftool"
+	manifest2 "github.com/tetratom/cftool/pkg/manifest"
+	"github.com/tetratom/cftool/pkg/pprint"
+	"os"
+	"path/filepath"
+)
+
+func Deploy(c context.Context, globalOpts GlobalOptions, deployOpts DeployOptions) (err error) {
+	// todo: show whoami
+
+	api, err := globalOpts.AWS.CloudFormationClient()
+	if err != nil {
+		return
+	}
+
+	manifestPath := deployOpts.ManifestFile
+	if manifestPath == "" {
+		manifestPath, err = findManifest()
+		if err != nil {
+			return
+		}
+	}
+
+	pprint.Field(color.Output, "Manifest", manifestPath)
+
+	manifest, err := manifest2.ReadFromFile(manifestPath)
+	if err != nil {
+		return
+	}
+
+	if err = os.Chdir(filepath.Dir(manifestPath)); err != nil {
+		return
+	}
+
+	var deployments []*cftool.Deployment
+
+	if deployment, ok, err := manifest.FindDeployment(deployOpts.Tenant, deployOpts.Stack); err != nil {
+		return err
+	} else if ok {
+		deployments = append(deployments, deployment)
+	}
+
+	for i, deployment := range deployments {
+		if i > 0 {
+			fmt.Fprint(color.Output, "\n")
+
+			deployer := internal.NewDeployer(api, deployment)
+			deployer.ShowDiff = deployOpts.ShowDiff
+
+			// todo: show whoami
+			// todo: check account and region match
+
+			if !deployment.Protected && !deployOpts.Yes {
+				deployment.Protected = true
+			}
+
+			if err = deployer.Deploy(color.Output); err != nil {
+				return errors.Wrapf(err, "deploy stack: %s", deployment.StackName)
+			}
+		}
+	}
+
+	return nil
+}
+
+func findManifest() (string, error) {
+	panic("not implemented")
+}
